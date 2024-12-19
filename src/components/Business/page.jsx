2d54af3b955f644bb191e839ab7dbf88b9223d96
Emon <emon50666@gmail.com>
2024-12-19 22:46:16 +0600

    why choose us section done

--- src/components/Business/page.jsx
+++ src/components/Business/page.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Business() {
   return (
-    <section className="relative flex flex-col-reverse lg:flex-row items-center justify-between px-4 py-10 md:px-8 lg:px-8">
+    <section className="relative flex flex-col-reverse lg:flex-row gap-4 justify-between px-4 py-10 md:px-8 lg:px-8">
       {/* Background Gradient */}
       <div
         className="absolute inset-0 z-0"
@@ -14,45 +14,78 @@ export default function Business() {
     
 
       {/* Content */}
-      <div className="relative z-10   mt-16 lg:mt-10
-      text-center lg:text-left max-w-md text-[#ffe6fe]">
-        <h1 className="text-3xl md:text-5xl font-bold capitalize mb-6"> <span className="bg-gradient-to-r from-[#4639BC] via-[rgb(254,229,134)] to-[#BC96F5] bg-clip-text text-transparent">Navigate</span> the Future of 
-          Trading with <span className="bg-gradient-to-r from-[#BC96F5] via-[rgb(254,229,134)] to-[#4639BC] bg-clip-text text-transparent">
-  Crypto Vision
-</span>
-  </h1>
-        <p className="mb-6 text-[#e3dcffb8]">
-          Single.earth Makes Nature Protection Profitable And Accessible For Everyone With Nature-backed Merit Tokens.
-        </p>
-        <div className="lg:flex  gap-3  ">
-          {/* Contact Us Button */}
-          <button
-            className="relative text-white font-semibold py-5   px-8 bg-gradient-to-r 
-             from-[#824FAD] via-[#4639BC] to-[#BC96F5] hover:from-[#6F26FF] hover:to-[#BC96F5]  transition-all duration-500"
-            style={{
-              clipPath: "polygon(15% 20%, 96% 22%, 96% 57%, 83% 85%, 5% 80%, 7% 45%)",
-            }}
-          >
-              Sign up for free
-          </button>
-
-          {/* Learn More Button */}
-
-          <button
-            className="relative  text-white  font-semibold py-5 px-8 
-            bg-gradient-to-r 
-             from-[#824FAD] via-[#4639BC] to-[#BC96F5] hover:from-[#6F26FF] 
-             hover:to-[#BC96F5] transition-all duration-500"
-            style={{
-              clipPath: "polygon(15% 20%, 96% 22%, 96% 57%, 83% 85%, 5% 80%, 7% 45%)",
-              boxShadow: "0 0 0 4px #5D22FF", // Simulated border with box-shadow
-            }}
-          >
-          Connect  wallet
-          </button>
-
+      <div className=" z-10    pr-5
+      text-center lg:text-left max-w-2xl text-[#ffe6fe]"> <h1 className="text-lg md:text-3xl font-semibold  capitalize mb-6"> Why Choose Crypto Flow </h1>
+      <div className='flex gap-4'>
+      <Image
+          src="/image.png"
+          alt="Right Side Image"
+          width={100}
+          height={100}
+          className="  max-w-[30px] h-[30px]  "
+          priority
+        />
+        <div>
+       <h1 className='text-lg text-gray-100 '>Fortified Security Protocols</h1>
+       <p className='text-[#e3dcffb8] text-[13px] lg:text-[15px] mb-2'>Your security is our topmost concern. We employ state-of-the-art security protocols, setting an industry standard to safeguard your assets and personal information.</p>
         </div>
-
+      </div>
+      <div className='flex gap-4'>
+      <Image
+          src="/image1.png"
+          alt="Right Side Image"
+          width={100}
+          height={100}
+          className="  max-w-[30px] h-[30px]  "
+          priority
+        />
+        <div>
+       <h1 className='text-lg text-gray-100 '>Performance-Driven Technology</h1>
+       <p className='text-[#e3dcffb8] text-[13px] lg:text-[15px] mb-2'>Our platform harnesses cutting-edge technology, delivering unparalleled performance and ensuring you have the tools needed for a seamless trading experience.</p>
+        </div>
+      </div>
+      <div className='flex gap-4'>
+      <Image
+          src="/image2.png"
+          alt="Right Side Image"
+          width={100}
+          height={100}
+          className="  max-w-[30px] h-[30px]  "
+          priority
+        />
+        <div>
+       <h1 className='text-lg text-gray-100 '>Responsive Customer Support</h1>
+       <p className='text-[#e3dcffb8] text-[13px] lg:text-[15px] mb-2'>Experience unmatched support with our responsive customer service team. We're here to address your inquiries promptly, ensuring a smooth and hassle-free experience.</p>
+        </div>
+      </div>
+      <div className='flex gap-4'>
+      <Image
+          src="/image3.png"
+          alt="Right Side Image"
+          width={100}
+          height={100}
+          className="  max-w-[30px] h-[30px]  "
+          priority
+        />
+        <div>
+       <h1 className='text-lg text-gray-100 '>Innovative Trading Features</h1>
+       <p className='text-[#e3dcffb8] text-[13px] lg:text-[15px] mb-2'>Stay ahead of the market curve with innovative trading features. Our platform introduces tools that empower you to make informed decisions and optimize your investment strategy.</p>
+        </div>
+      </div>
+      <div className='flex gap-4'>
+      <Image
+          src="/image4.png"
+          alt="Right Side Image"
+          width={100}
+          height={100}
+          className="  max-w-[30px] h-[30px]  "
+          priority
+        />
+        <div>
+       <h1 className='text-lg text-gray-100 '>Transparent Fee Structure</h1>
+       <p className='text-[#e3dcffb8] text-[13px] lg:text-[15px] mb-2'>Say goodbye to hidden fees. Our transparent fee structure ensures you are fully informed about costs, allowing you to manage your investments with clarity and confidence.</p>
+        </div>
+      </div>
       </div>
       {/* Right Image */}
       <div className="relative z-10  hidden sm:hidden lg:block xl:block  w-full max-w-sm lg:max-w-md ">
